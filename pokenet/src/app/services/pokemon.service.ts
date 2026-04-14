d0b26f885f3977ed5dbe432b8478f33f164f7fb1
Liang-cmd777 <draftsman369@gmail.com>
2026-04-15 01:37:26 +0200

    pokemon_service-commit

---
+++ pokenet/src/app/services/pokemon.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Pokemon } from '../models/pokemon.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+
+  private pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'Bulbasaur',
+      type: 'Grass/Poison',
+      description: 'A calm Pokémon with a plant bulb on its back.',
+    },
+    {
+      id: 4,
+      name: 'Charmander',
+      type: 'Fire',
+      description: 'A fire Pokémon with a flame on its tail.',
+    },
+    {
+      id: 7,
+      name: 'Squirtle',
+      type: 'Water',
+      description: 'A small turtle Pokémon that shoots water.',
+    },
+    {
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      description: 'An electric Pokémon that stores energy in its cheeks.',
+    },
+  ]
+
+  getAll(): Pokemon[] {
+    return this.pokemons;
+  }
+
+  getById(id: number): Pokemon | undefined {
+    return this.pokemons.find(pokemon => pokemon.id === id);
+  }
+}
